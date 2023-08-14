6cc0f48ff4667304e340f5c38b59e7d74cb89e83
최한슬 <hanseulo@naver.com>
2023-08-14 18:42:26 +0900

    react 23-08-14

---
+++ 03tut/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  const handleNameChange = () => {
+    const names = ["Winter", "Karina", "Ningning"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03tut/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>식품 리스트</h1>
+    </header>
+  )
+}
+
+export default Header
