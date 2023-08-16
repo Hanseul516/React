3045b11444fec9ad1823df372d59dc4b8cfb9fea
최한슬 <hanseulo@naver.com>
2023-08-16 16:26:24 +0900

    react 23-08-16

--- 04tut/src/Content.js
+++ 04tut/src/Content.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState("Seul");
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ["Winter", "Karina", "Ning"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
+  };
+
+  const handleClick = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  const handleClick2 = () => {
+    console.log(count);
   };
 
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
     </main>
   );
 };
