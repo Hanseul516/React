c0b39757e09c7dcf11afb301bbc7117a65888118
최한슬 <hanseulo@naver.com>
2023-08-21 10:40:24 +0900

    react

--- 04tut/src/Content.js
+++ 04tut/src/Content.js
@@ -1,64 +1,13 @@
-import { useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
+import ItemList from "./ItemList";
 
-const Content = () => {
-  const [items, setitems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "솔티드 초콜릿",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "item2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "item3",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setitems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setitems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                onChange={() => handleCheck(item.id)}
-                checked={item.checked}
-              />
-              <label
-                style={item.checked ? { textDecoration: "line-through" } : null}
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
-              <FaTrashAlt
-                onClick={() => handleDelete(item.id)}
-                role="button"
-                tabIndex="0"
-              />
-            </li>
-          ))}
-        </ul>
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete} />
       ) : (
         <p style={{ marginTop: "2em" }}>리스트가 비었습니다.</p>
       )}
