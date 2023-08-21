d6468b1a65440832d2c20b056b7dfc18d074e800
최한슬 <hanseulo@naver.com>
2023-08-21 14:03:32 +0900

    react

---
+++ 04tut/src/SearchItem.js
@@ -0,0 +1,17 @@
+const SearchItem = ({search, setSearch}) => {
+    return (
+        <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="search">Search</label>
+            <input
+                id="search"
+                type="text"
+                role="searchbox"
+                placeholder="검색하기" 
+                value={search}
+                onChange={(e)=> setSearch(e.target.value)}
+                />
+        </form>
+    )
+}
+
+export default SearchItem
