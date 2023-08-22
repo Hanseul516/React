31fa038b5bcc2df71dcf7c919e593f67a25226c8
최한슬 <hanseulo@naver.com>
2023-08-22 17:44:08 +0900

    react

--- reactList/src/App.js
+++ reactList/src/App.js
@@ -6,13 +6,31 @@ import Footer from "./Footer";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const API_URL = 'http://localhost:3500/items'
+
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
 
   useEffect(() => {
-    localStorage.setItem("shoppinglist", JSON.stringify(items));
-  }, [items])
+
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL); //fetch 함수를 사용하여 API_URL에서 데이터를 요청
+        if (!response.ok) throw Error('예상 데이터를 수신하지 못함');
+        const listItems = await response.json(); //API 응답을 JSON 형식으로 변환하여 listItems 변수에 저장
+        console.log(listItems);
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message)
+      }
+    }
+    setTimeout(() => {
+      fetchItems();
+    }, 2000)
+  }, [])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
@@ -46,14 +64,21 @@ function App() {
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
-        handleSubmit={handleSubmit} />
+        handleSubmit={handleSubmit}
+      />
       <SearchItem
         search={search}
-        setSearch={setSearch} />
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete} />
+        setSearch={setSearch}
+      />
+      <main>
+        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}
+        </p>}
+        {!fetchError && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
