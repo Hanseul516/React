32644d41ac1957caf03bc3e463262e675cd2f088
최한슬 <hanseulo@naver.com>
2023-08-30 10:11:11 +0900

    react

--- reactRouter/src/App.js
+++ reactRouter/src/App.js
@@ -6,9 +6,8 @@ import NewPost from './NewPost';
 import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
-import { Route, Switch, useHistory } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { format } from 'date-fns';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -35,63 +34,22 @@ function App() {
       title: "My Fourth Post",
       datetime: "July 01, 2021 11:17:36 AM",
       body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    }
-  ])
-  const [search, setSearch] = useState('');
+    }])
+
+  const [search, setSearch] = useState('')
   const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-  const history = useHistory();
-
-  useEffect(() => {
-    const filteredResults = posts.filter((post) =>
-      ((post.body).toLowerCase()).includes(search.toLowerCase())
-      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
-
-    setSearchResults(filteredResults.reverse());
-  }, [posts, search])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-    const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    history.push('/');
-  }
-
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    history.push('/');
-  }
 
   return (
-    <div className="App">
-      <Header title="React JS Blog" />
+    <div className='App'>
+      <Header title="React DS Blog" />
       <Nav search={search} setSearch={setSearch} />
-      <Switch>
-        <Route exact path="/">
-          <Home posts={searchResults} />
-        </Route>
-        <Route exact path="/post">
-          <NewPost
-            handleSubmit={handleSubmit}
-            postTitle={postTitle}
-            setPostTitle={setPostTitle}
-            postBody={postBody}
-            setPostBody={setPostBody}
-          />
-        </Route>
-        <Route path="/post/:id">
-          <PostPage posts={posts} handleDelete={handleDelete} />
-        </Route>
-        <Route path="/about" component={About} />
-        <Route path="*" component={Missing} />
-      </Switch>
+      <Routes>
+        <Route exact path='/' element={<Home posts={posts} />} />
+        <Route exact path='/post' element={<NewPost />} />
+        <Route path='/post/:id' element={<PostPage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<Missing />} />
+      </Routes>
       <Footer />
     </div>
   );
--- reactRouter/src/Home.js
+++ reactRouter/src/Home.js
@@ -1,8 +1,13 @@
-import React from 'react'
-
-const Home = () => {
+const Home = ({ posts }) => {
     return (
-        <main>
+        <main className="Home">
+            {posts.length ? (
+                <Feed posts={posts} />
+            ) : (
+                <p style={{ marginTop: "2rem" }}>
+                    표시할 게시물이 없습니다.
+                </p>
+            )}
             <h1>Home</h1>
         </main>
     )
